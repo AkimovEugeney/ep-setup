85a0b521732d4c9e815036728e5dc75216308cd2
Eugeney <akimoveugeney@gmail.com>
2025-03-12 13:13:09 +0300

    fix: Language

--- index.js
+++ index.js
@@ -8,21 +8,17 @@ const templates = [
   { src: path.join(__dirname, '.eslintrc.template.js'), dest: '.eslintrc.js' }
 ];
 
-// Копируем шаблоны и перезаписываем существующие файлы
 templates.forEach(({ src, dest }) => {
   const destPath = path.join(process.cwd(), dest);
-
-  // Всегда перезаписываем файл
   fs.copyFileSync(src, destPath);
-  console.log(`✅ ${dest} перезаписан из шаблона!`);
+  console.log(`✅ ${dest} has been updated from the template!`);
 });
 
-// Устанавливаем зависимости
-console.log('📦 Устанавливаем ESLint и Prettier...');
+console.log('📦 Installing ESLint and Prettier...');
 const { execSync } = require('child_process');
 execSync(
   'npm install --save-dev eslint prettier @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint-plugin-prettier prettier-plugin-tailwindcss @trivago/prettier-plugin-sort-imports',
   { stdio: 'inherit' }
 );
 
-console.log('🚀 ESLint и Prettier успешно настроены!');
+console.log('🚀 ESLint and Prettier have been successfully configured!');
